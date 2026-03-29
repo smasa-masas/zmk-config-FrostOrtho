4fc915ce2e8edc0ab5f5cae4719f09b2981ae37f
smasa-masas <2032302matsu@gmail.com>
2026-03-29 12:34:05 +0900

    Update MOUSE layer: Change H key to left click (MB1) and U key to right click (MB3)

---
+++ keymap.c
@@ -0,0 +1,10 @@
+// Updated MOUSE layer in keymap
+const keymap = {
+  // other layers...
+  MOUSE: {
+    // other keys...
+    [15]: MB1, // H key
+    [7]: MB3, // U key
+  },
+  // other layers...
+};
